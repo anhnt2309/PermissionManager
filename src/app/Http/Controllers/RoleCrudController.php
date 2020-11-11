bd86da0a6dc7edd68a7746d134d7f66e613c0556
Tuan_Anh <anhnt.2309@gmail.com>
2020-11-11 15:39:20 +0700

    Adjust role crud

--- src/app/Http/Controllers/RoleCrudController.php
+++ src/app/Http/Controllers/RoleCrudController.php
@@ -87,7 +87,7 @@ class RoleCrudController extends CrudController
         $this->crud->addColumn([
             // n-n relationship (with pivot table)
             'label' => ucfirst(trans('backpack::permissionmanager.permission_plural')),
-            'type' => 'permissions',
+            'type' => 'select_multiple',
             'name' => 'permissions', // the method that defines the relationship in your Model
             'entity' => 'permissions', // the method that defines the relationship in your Model
             'attribute' => 'name', // foreign key attribute that is shown to user
@@ -133,7 +133,7 @@ class RoleCrudController extends CrudController
 
         $this->crud->addField([
             'label' => ucfirst(trans('backpack::permissionmanager.permission_plural')),
-            'type' => 'checklist',
+            'type' => 'permissions',
             'name' => 'permissions',
             'entity' => 'permissions',
             'attribute' => 'name',
